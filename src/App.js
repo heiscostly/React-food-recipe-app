97c9c1652667df03b206660993cc7710d96226cd
kedehcostly <ayangcostly@gmail.com>
2020-11-13 15:34:57 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import react, { useEffect, useState } from "react";
 import './App.css';
+import Recipe from "./recipe.js";
 
 function App() {
+  const APP_ID ="e1618d03"
+  const APP_KEY="8c7afa5c6245d4fe401c713903824dca"
+
+  // const exampleReq=
+  const [recipe, setRecipes] = useState([]);
+
+  useEffect( () => {
+    getRecipe();
+  }, []);
+
+  const getRecipe = async () => {
+
+    const response = await fetch(`https://api.edamam.com/search?q=rice&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    setRecipes(data.hits);
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <form className="search-form">
+    <input className="search-bar" type="text"/>
+    <button className="search-button" type="submit">Search</button>
+    </form>
+    {recipe.map(recipe =>(
+      <recipe />
+    ))};
     </div>
   );
 }
