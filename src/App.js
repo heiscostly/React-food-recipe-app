35c1ca182d874d86911014135e3fa1cb8bcac112
kedehcostly <ayangcostly@gmail.com>
2020-11-16 10:58:56 +0100

    My Final Pushing

--- src/App.js
+++ src/App.js
@@ -40,18 +40,23 @@ function App() {
   const getSearch = e =>{
     e.preventDefault();
     setQuery(search);
+    setSearch("")
   }
 
 
   return (
     <div className="App">
-    <form onSubmit={getSearch} className="search-form">
-    <input className="search-bar" type="text" value ={search} onChange ={updateSearch}/>
-    <button className="search-button" type="submit">Search</button>
-    </form>
-    {recipe.map(recipe =>(
-      <Recipe key={recipe.recipe.label} tittle={recipe.recipe.label} calories={recipe.recipe.calories} image={recipe.recipe.image}/>
-    ))}
+      <form onSubmit={getSearch} className="search-form">
+      <input className="search-bar" type="text" value ={search} onChange ={updateSearch}/>
+      <button className="search-button" type="submit">Search</button>
+      </form>
+
+
+      <div className= "recip">
+        {recipe.map(recipe =>(
+          <Recipe key={recipe.recipe.label} tittle={recipe.recipe.label} calories={recipe.recipe.calories} image={recipe.recipe.image} ingredients ={recipe.recipe.ingredients}/>
+        ))}
+      </div>
     </div>
   );
 }
--- src/recipe.js
+++ src/recipe.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import style from "./recipe.module.css"
 
 
-function recipe ({tittle,calories,image}){
+function recipe ({tittle,calories,image, ingredients}){
   return (
-    <div>
+    <div className={style.recip}>
     <h1>{tittle}</h1>
+    {ingredients.map(ingredient => (
+      <li>{ingredient.text}</li>
+    ))}
     <p> No of Calories: {calories}</p>
     <img src={image} alt="myimage" />
 
