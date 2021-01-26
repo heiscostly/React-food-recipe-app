7c88291fd2924e2c28043c06ddfa32d2e1c7088c
Heiscostly <ayangcostly@gmail.com>
2021-01-26 11:42:17 +0100

    Moved API_ID & API_KEY to env file

--- src/App.js
+++ src/App.js
@@ -3,25 +3,24 @@ import './App.css';
 import Recipe from "./recipe.js";
 
 function App() {
-  const APP_ID = "6e52ef54";
-  const APP_KEY= "7c208dbf6d433ee1a649d0acb336fd56";
+  const { REACT_APP_API_ID: APP_ID, REACT_APP_API_KEY: APP_KEY } = process.env;
 
   // const exampleReq=
   const [recipe, setRecipes] = useState([]);
-  const [search, setSearch] =  useState('');
+  const [search, setSearch] = useState('');
   const [query, setQuery] = useState("banana");
 
 
 
 
 
-  useEffect( () => {
+  useEffect(() => {
     getRecipe()
   }, [query]);
 
   const getRecipe = async () => {
     const proxyurl = "https://cors-anywhere.herokuapp.com/";
-    const response = await fetch( proxyurl + `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const response = await fetch(proxyurl + `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json()
     setRecipes(data.hits);
     console.log(data.hits);
@@ -37,7 +36,7 @@ function App() {
 
 
 
-  const getSearch = e =>{
+  const getSearch = e => {
     e.preventDefault();
     setQuery(search);
     setSearch("")
@@ -47,14 +46,14 @@ function App() {
   return (
     <div className="App">
       <form onSubmit={getSearch} className="search-form">
-      <input className="search-bar" type="text" value ={search} onChange ={updateSearch}/>
-      <button className="search-button" type="submit">Search</button>
+        <input className="search-bar" type="text" value={search} onChange={updateSearch} />
+        <button className="search-button" type="submit">Search</button>
       </form>
 
 
-      <div className= "recip">
-        {recipe.map(recipe =>(
-          <Recipe key={recipe.recipe.label} tittle={recipe.recipe.label} calories={recipe.recipe.calories} image={recipe.recipe.image} ingredients ={recipe.recipe.ingredients}/>
+      <div className="recip">
+        {recipe.map(recipe => (
+          <Recipe key={recipe.recipe.label} tittle={recipe.recipe.label} calories={recipe.recipe.calories} image={recipe.recipe.image} ingredients={recipe.recipe.ingredients} />
         ))}
       </div>
     </div>
