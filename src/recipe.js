26b64f30030a178a65cb854aef4ee760be13998d
Heiscostly <ayangcostly@gmail.com>
2021-01-26 11:56:55 +0100

    Fixed Some Errors showing in the console

--- src/recipe.js
+++ src/recipe.js
@@ -2,15 +2,19 @@ import React from 'react';
 import style from "./recipe.module.css"
 
 
-function recipe ({tittle,calories,image, ingredients}){
+function recipe({ tittle, calories, image, ingredients }) {
   return (
     <div className={style.recip}>
-      <h1 style={{display:"flex",color:"black", fontSize:"1rem", padding:"10px"}}>{tittle}</h1>
-      <p style={{padding:"10px", display:"flex", flexDirection:"column", justifyContent:"center", alignItems:"center", listStyle:"none"}}> <h2>INGREDIENTS</h2>{ingredients.map(ingredient => (
-        <li>{ingredient.text}</li>
-      ))}</p>
-      <p> <h2>CALORIES</h2> {calories}</p>
-      <img style={{width: "40%", borderRadius:"10px", height:""}} src={image} alt="myimage" />
+      <h1 style={{ display: "flex", color: "black", fontSize: "1rem", padding: "10px" }}>{tittle}</h1>
+      <div style={{ padding: "10px", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", listStyle: "none" }}>
+        <h2>INGREDIENTS</h2>
+        {ingredients.map(({ text }, index) => (
+          <li key={index.toString()}>{text}</li>
+        ))}
+      </div>
+      <h2>CALORIES</h2>
+      <p>{calories}</p>
+      <img style={{ width: "40%", borderRadius: "10px", height: "" }} src={image} alt="myimage" />
 
     </div>
   );
