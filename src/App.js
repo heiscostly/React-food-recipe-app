fdeaf58c8af0feb6217488d8b4a1b870937f7a60
Costly Ayang <39498038+heiscostly@users.noreply.github.com>
2021-06-17 14:41:59 +0100

    Made Some Changes
    Made some changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import react, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import './App.css';
 import Recipe from "./recipe.js";
 
 function App() {
-  const { REACT_APP_API_ID: APP_ID, REACT_APP_API_KEY: APP_KEY } = process.env;
+  // const { REACT_APP_API_ID: APP_D, REACT_APP_API_KEY: APP_EY } = process.env;
+  
+  console.log(process.env, "Hey Dude")
+
+
+  
+  const APP_ID = process.env.REACT_APP_API_ID
+  const APP_KEY = process.env.REACT_APP_API_KEY
 
   // const exampleReq=
   const [recipe, setRecipes] = useState([]);
@@ -16,18 +23,27 @@ function App() {
 
 
   useEffect(() => {
+    const getRecipe = async () => {
+      setLoading(true);
+      // const proxyurl = "https://cors-anywhere.herokuapp.com/";
+      const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+      const data = await response.json()
+      setRecipes(data.hits);
+      setLoading(false);
+      console.log(data.hits);
+    };
     getRecipe()
-  }, [query]);
+  }, [APP_ID, APP_KEY, query]);
 
-  const getRecipe = async () => {
-    setLoading(true);
-    const proxyurl = "https://cors-anywhere.herokuapp.com/";
-    const response = await fetch(proxyurl + `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
-    const data = await response.json()
-    setRecipes(data.hits);
-    setLoading(false);
-    console.log(data.hits);
-  };
+  // const getRecipe = async () => {
+  //   setLoading(true);
+  //   // const proxyurl = "https://cors-anywhere.herokuapp.com/";
+  //   const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+  //   const data = await response.json()
+  //   setRecipes(data.hits);
+  //   setLoading(false);
+  //   console.log(data.hits);
+  // };
 
 
 
@@ -49,7 +65,7 @@ function App() {
   return (
     <div className="App">
       <form onSubmit={getSearch} className="search-form">
-        <input className="search-bar" type="text" value={search} onChange={updateSearch} />
+        <input className="search-bar" type="text" value={search} onChange={updateSearch} style={{borderRadius:"4px"}} />
         <button className="search-button" type="submit">Search</button>
       </form>
 
--- src/recipe.js
+++ src/recipe.js
@@ -5,16 +5,16 @@ import style from "./recipe.module.css"
 function recipe({ tittle, calories, image, ingredients }) {
   return (
     <div className={style.recip}>
-      <h1 style={{ display: "flex", color: "black", fontSize: "1rem", padding: "10px" }}>{tittle}</h1>
+      <h1 style={{ display: "flex", color: "black", fontSize: "1rem", padding: "0" }}>{tittle}</h1>
       <div style={{ padding: "10px", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", listStyle: "none" }}>
-        <h2>INGREDIENTS</h2>
+        <h2 style={{margin:"0"}}>INGREDIENTS</h2>
         {ingredients.map(({ text }, index) => (
           <li key={index.toString()}>{text}</li>
         ))}
       </div>
-      <h2>CALORIES</h2>
-      <p>{calories}</p>
-      <img style={{ width: "40%", borderRadius: "10px", height: "" }} src={image} alt="myimage" />
+      {/* <h2>CALORIES</h2>
+      <p>{calories}</p> */}
+      <img style={{ width: "40%", borderRadius: "10px", height: "", paddingBottom:"10px" }} src={image} alt="myimage" />
 
     </div>
   );
